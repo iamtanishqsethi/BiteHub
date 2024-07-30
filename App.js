7edc1dadc45f80c560261801cacb6dc8c0745c53
Tanishq Sethi <tanishqsethi05@gmail.com>
2024-07-30 11:43:48 +0530

    namste react ep 1:

---
+++ App.js
@@ -0,0 +1,25 @@
+
+//creating nested elements
+const parent=React.createElement("div",
+    {id:"parent"},
+    [React.createElement("div",
+        {id:"child1"},
+        [React.createElement("h1",
+            {},"I am an h1 tag"
+        ),React.createElement("h2",{},"I am an h2 tag")]
+    ),React.createElement("div",
+        {id:"child2"},
+        [React.createElement("h1",
+            {},"I am an h1 tag"
+        ),React.createElement("h2",{},"I am an h2 tag")]
+    )]
+);
+console.log(parent);
+//creating a h1 using react
+
+// const heading=React.createElement("h1",{id:"heading"},"Hello World from react");//it takes 3 arguments the html element,an object and the content inside the element
+//first of all react needs a root , where we can work
+//its the job of react dom
+//root is the place where all the react code will work/render
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
