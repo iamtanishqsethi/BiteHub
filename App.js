5f7334eb61391f686f28c61617ae80933ef55eff
Tanishq Sethi <tanishqsethi05@gmail.com>
2024-08-02 17:03:17 +0530

    namaste react ep2

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 //creating nested elements
 const parent=React.createElement("div",
     {id:"parent"},
     [React.createElement("div",
         {id:"child1"},
         [React.createElement("h1",
-            {},"I am an h1 tag"
+            {},"This is Namaste React 💀"
         ),React.createElement("h2",{},"I am an h2 tag")]
     ),React.createElement("div",
         {id:"child2"},
