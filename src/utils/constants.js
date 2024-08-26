cd2ce6c0b5beef3802cd8b83fdd4bc8b0534248c
Tanishq Sethi <tanishqsethi05@gmail.com>
2024-08-26 17:46:18 +0530

    image id issue resolved

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const CDN_URL='https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/'
+export const CDN_URL='https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/'
 
 export const LOGO_URL="https://images-platform.99static.com/A_Ax0GQuo_NHI0Y7XZHmFtGfBDY=/0x0:1000x1000/500x500/top/smart/99designs-contests-attachments/126/126252/attachment_126252018"
 //for multiple exports we cannot use default export we use named export
